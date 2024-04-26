f46350a85a1cf6f2655dfd1ffbc1ed2c976894f6
balex <balexvicx@gmail.com>
2024-04-26 21:53:15 +0200

    Scaffold

--- app/src/main/java/com/balex/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/balex/vknewsclient/MainActivity.kt
@@ -1,77 +1,169 @@
 package com.balex.vknewsclient
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Test()
+            //Test()
+            TestDrawer()
         }
     }
 }
 
+@Composable
+private fun TestDrawer() {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    //val drawerState = rememberDrawerState(initialValue = DrawerValue.Open)
+    val scope = rememberCoroutineScope()
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Text(text = "Text 1")
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = "Text 2")
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = "Text 3")
+            }
+        },
+    ) {
+        Test()
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun Test() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
-    ) {
-        Example3()
-    }
-}
+    //val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    Scaffold(
+        //modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
 
-@Composable
-private fun Example1() {
-    OutlinedButton(onClick = {}) {
-    //Button(onClick = {}) {
-        Text(text = "Hello World")
-    }
-}
-
-@Composable
-private fun Example2() {
-    TextField(
-        value = "Value",
-        onValueChange = {},
-        label = { Text(text = "Label") }
-    )
-}
-
-@Composable
-private fun Example3() {
-    AlertDialog(
-        onDismissRequest = { Log.d("diss", "miss")},
-        confirmButton = {
-            Text(
-                modifier = Modifier.padding(8.dp),
-                text = "Yes"
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        "Centered Top App Bar",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                //scrollBehavior = scrollBehavior,
             )
         },
-        title = {
-            Text(
-                text = "Are you sure?"
+
+        bottomBar = {
+            BottomAppBar(
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(Icons.Filled.Check, contentDescription = "Localized description")
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.Edit,
+                            contentDescription = "Localized description",
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.Call,
+                            contentDescription = "Localized description",
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.Face,
+                            contentDescription = "Localized description",
+                        )
+                    }
+                },
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = { /* do something */ },
+                        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+                    ) {
+                        Icon(Icons.Filled.Add, "Localized description")
+                    }
+                }
             )
         },
-        text = {
-            Text(text = "Do you want to delete this file?")
-        },
-        dismissButton = {
-            Text(
-                modifier = Modifier.padding(8.dp),
-                text = "No"
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                text = { Text("Show drawer") },
+                icon = { Icon(Icons.Filled.Add, contentDescription = "") },
+                onClick = {
+//                    scope.launch {
+//                        drawerState.apply {
+//                            if (isClosed) open() else close()
+//                        }
+//                    }
+                }
             )
         }
     )
+
+    {
+        Text(
+            modifier = Modifier.padding(it),
+            text = "this is Scaffold content"
+        )
+    }
 }
