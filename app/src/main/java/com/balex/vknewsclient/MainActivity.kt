b221e8a502cde0a50f5ab015f93f2d4ef98103e3
balex <balexvicx@gmail.com>
2024-05-09 23:11:09 +0200

    LaunchedEffect, LaunchedEffect

--- app/src/main/java/com/balex/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/balex/vknewsclient/MainActivity.kt
@@ -6,19 +6,29 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import com.balex.vknewsclient.ui.theme.ActivityResultTest
-import com.balex.vknewsclient.ui.theme.MainScreen
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.balex.vknewsclient.ui.theme.VkNewsClientTheme
 import com.vk.api.sdk.VK
 import com.vk.api.sdk.auth.VKAuthenticationResult
-import com.vk.api.sdk.auth.VKScope
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var t = 5
         setContent {
             VkNewsClientTheme {
+                val someState = remember {
+                    mutableStateOf(true)
+                }
+                //t++
+                Log.d("MainActivity", "Recomposition: ${someState.value}")
                 val launcher = rememberLauncherForActivityResult(
                     contract = VK.getVKAuthActivityResultContract()
                 ) {
@@ -31,8 +41,16 @@ class MainActivity : ComponentActivity() {
                         }
                     }
                 }
-                launcher.launch(listOf(VKScope.WALL))
-                MainScreen()
+                LaunchedEffect(key1 = t ) {
+                    Log.d("MainActivity", "LaunchedEffect")
+                    delay(100)
+                }
+                SideEffect {
+                    Log.d("MainActivity", "SideEffect")
+                }
+                Button(onClick = { someState.value = !someState.value }) {
+                    Text(text = "Change state")
+                }
             }
         }
     }
