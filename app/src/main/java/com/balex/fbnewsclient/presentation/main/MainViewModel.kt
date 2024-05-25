82d76debd7ad777e0dac2bf19cdc5e0db0444c22
balex <balexvicx@gmail.com>
2024-05-25 22:10:19 +0200

    di 1

--- app/src/main/java/com/balex/fbnewsclient/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/balex/fbnewsclient/presentation/main/MainViewModel.kt
@@ -1,7 +1,6 @@
 package com.balex.fbnewsclient.presentation.main
 
 import android.app.Activity
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.balex.fbnewsclient.domain.entity.AuthState
